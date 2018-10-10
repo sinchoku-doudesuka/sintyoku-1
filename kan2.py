2e49f8005bc0d02b01c5bc74b1311de2cf8e59e7
tnk14Games <tnk14games@gmail.com>
2018-10-10 22:02:50 +0900

    first import

---
+++ kan2.py
@@ -0,0 +1,82 @@
+from pyzbar.pyzbar import decode
+from PIL import Image
+import cv2
+import requests
+import webbrowser
+import tkinter
+import tkinter.filedialog
+import os
+
+data = None
+
+capture = cv2.VideoCapture(0)
+
+while not data:
+
+    ret, image = capture.read()
+
+    image = cv2.flip(image,1)
+
+    cv2.imshow('QRcodeReader', image)
+
+    image = cv2.flip(image,1)
+
+    if cv2.waitKey(1) >= 0:
+        # GUIでファイル読み込み
+        show = tkinter.Tk()
+        show.withdraw()
+        image = Image.open(tkinter.filedialog.askopenfilename(filetypes=[('image files','*.png')]))
+        show.destroy()
+        ret = True
+
+    if ret == True:
+        # QRコードの読取り
+        data = decode(image)
+
+capture.release()
+cv2.destroyAllWindows()
+
+#デバック用プリント            
+print(data[0][0].decode('utf-8', 'ignore'))
+
+URL=data[0][0].decode('utf-8', 'ignore')
+
+if not  URL.count('http'):
+    URL='http://'+URL
+    print(URL)
+
+payload={
+	"browserHeight":"683",
+	"browserWidth":"1138",
+	"url":URL,
+	"waitTime":"1"
+}
+
+r=requests.post('https://securl.nu/jx/get_page_jx.php',data=payload)
+
+hash=r.json()
+
+print("check marware")
+try:
+	result=hash['viruses'][0]['type']
+
+except IndexError:
+	print("virus not included")
+
+else:
+	print("---warning---")
+	print("This site includes marware!!")
+	print(result)
+
+
+print("check fishing")
+try:
+	result=hash['blackList'][0]['type']
+
+except IndexError:
+	print("safty site")
+
+else:
+	print("---warning---")
+	print("This site is fishing site!!")
+	print(result)
